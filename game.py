ce799552fd412eb1227ceff4c29d3b23e452d5c3
ildo.kim <aknife@naver.com>
2026-07-01 14:56:23 +0900

    [feature] page 40

--- game.py
+++ game.py
@@ -5,3 +5,7 @@ class Game:
             raise TypeError()
         if len(guessNumber) != 3:
             raise TypeError()
+
+        for num in guessNumber:
+            if not ord('0') <= ord(num) <= ord('9'):
+                raise TypeError()
--- test_game.py
+++ test_game.py
@@ -17,6 +17,7 @@ def test_exception_when_invalid_input(game):
     assert_illegal_argument(game, None)
     assert_illegal_argument(game, "12")
     assert_illegal_argument(game, "1234")
+    assert_illegal_argument(game, "12s")
 
 
 
